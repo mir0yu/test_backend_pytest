f1b2e82f55f699af7e3599b5a23ef1042c65a298
mirosh <peaceismir@gmail.com>
2021-12-05 02:23:16 +0300

    add 3 tests for api get/post/delete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-from secrets import SECRET_KEY, NAME, BASE_URL, AUTH_URL_FSPO, GROUPS_URL
+from secrets import SECRET_KEY, NAME, BASE_URL, AUTH_URL_FSPO, GROUPS_URL, PROFESSORS_URL, SCHEDULE_URL, DEADLINE_URL
 
 
 class User:  # User data may be implement with ArgParse/environment variable...
@@ -14,9 +14,6 @@ class User:  # User data may be implement with ArgParse/environment variable...
         self.response = self.auth()
         self.response_json = json.loads(self.response.text)
         self.session_id = self.response_json["result"]["sessionId"]  # assigning session_id from authentication
-        # print(f"session id : {self.session_id}; login : {self._login}; password : {self._password}")
-        # print(self.response.json())
-        # print(self.response.text)
         try:
             self.response.raise_for_status()
         except requests.exceptions.HTTPError as e:
@@ -35,17 +32,10 @@ class User:  # User data may be implement with ArgParse/environment variable...
 
 class TestUser:
     user_for_api = User()
-    # headers = {
-    #     'X-Session-Id': f'{user_for_api.session_id}',
-    #     'Content-type': 'application/json'
-    # }
 
     def test_groups(self):
         response = requests.get(BASE_URL + GROUPS_URL)
         response_json = json.loads(response.text)
-        # print(response.text)
-        # print(self.user.response.text)
-        # print(BASE_URL+GROUPS_URL)
         try:
             response.raise_for_status()
         except requests.exceptions.HTTPError as e:
@@ -57,6 +47,86 @@ class TestUser:
             assert isinstance(r["name"], str), "groups name must be string"
         return response.text
 
+    def test_professors(self):
+        response = requests.get(BASE_URL + PROFESSORS_URL)
+        response_json = json.loads(response.text)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print('ERROR: %s' % e)
+        assert response_json["error"] is None
+        assert response_json["result"] is not None
+        for r in response_json["result"]["items"]:
+            assert isinstance(r["_id"], str), "professor id must be string"
+            assert isinstance(r["name"], str), "professor name must be string"
+        return response.text
+
+    def test_schedule(self):
+        _id = self.user_for_api.response_json["result"]["user"]["scheduleUser"]["_id"]
+        response = requests.get(BASE_URL + SCHEDULE_URL + _id)
+        response_json = json.loads(response.text)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print('ERROR: %s' % e)
+        assert response_json["error"] is None
+        assert response_json["result"] is not None
+        for r in response_json["result"]["items"]:
+            assert isinstance(r["_id"], str), "student id must be string"
+            assert (r["day"] in ["sun", "mon", "tue", "wed", "thu", "fri", "sat"])
+            assert (r["week"] in ["odd", "even"])
+            assert isinstance(r["startTime"], str), "Start time must be string"
+            assert isinstance(r["endTime"], str), "End time must be string"
+            assert isinstance(r["lessonNum"], int), "lesson Number must be int"
+            assert (r["type"] in ["lecture", "lab", "practice", "test", "course", None])
+            assert isinstance(r["rooms"], str), "rooms must be string"
+
+        return response.text
+
+    def test_create_deadline(self, title="sdjad", description="dasd", date=2378123):
+        response = requests.post(
+            f"{BASE_URL}{DEADLINE_URL}",
+            data=json.dumps(
+                {"title": title,
+                 "description": description,
+                 "date": date}
+            ),
+            headers={
+                'Content-type': 'application/json',
+                'X-Session-Id': self.user_for_api.session_id
+            }
+        )
+        response_json = json.loads(response.text)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print('ERROR: %s' % e)
+        assert response_json["error"] is None
+        assert response_json["result"] is not None
+        return response_json["result"]["_id"]
+
+    def test_delete_deadline(self):
+        _id = self.test_create_deadline("Это мой 13 дедлайн", "Пока без описания, впредь осторожнее", 1628764738)
+        response = requests.delete(
+            f"{BASE_URL}{DEADLINE_URL}/{_id}/close",
+            headers={
+                'Content-type': 'application/json',
+                'X-Session-Id': self.user_for_api.session_id
+            }
+        )
+        response_json = json.loads(response.text)
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print('ERROR: %s' % e)
+        assert response_json["error"] is None
+        assert response_json["result"] is not None
+
 
 if __name__ == '__main__':
     print(TestUser().test_groups())
+    print(TestUser().test_professors())
+    print(TestUser().test_schedule())
+    print(TestUser().test_create_deadline(title="Это мой двенадцатый дедлайн",
+                                          description="Пока без описания, впредь осторожнее", date=1628764738))
+    print(TestUser().test_delete_deadline())
